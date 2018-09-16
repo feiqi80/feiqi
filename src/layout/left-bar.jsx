374d4fd0243c09196de6ce9b255506a616b063f8
feiqi80 <feiq@microseer.com>
2018-09-17 07:25:03 +0800

    commit

--- src/layout/left-bar.jsx
+++ src/layout/left-bar.jsx
@@ -1,105 +1,92 @@
 import React, { Component } from "react";
 
 class Leftbar extends Component {
-    menu = {
-        children: [
-            {name: "菜单1", url: "", isExpand: false, children: [
-                {name: "菜单1的子菜单", url: "/intro/:id"},
-            ]},
-            {name: "菜单2", url: "/calc"},
-            {name: "菜单3", url: "/game"}
-        ] 
-    }    
+    constructor() {
+        super();
+        this.state = {
+            menuList: [
+                {id: 1, name: "菜单1", url: "", isExpand: false, children: [
+                    {id: 2, name: "菜单1的子菜单", isActive: false,  url: "/intro/:id"},
+                ]},
+                {id: 3, name: "菜单2", isActive: false, url: "/calc"},
+                {id: 4, name: "菜单3", isActive: false, url: "/game"}
+            ]
+        }
+    }
+    
 
     clickMenu() {
 
     }
     
-    expandMenu = (obj) => {        
-        console.log(obj.isExpand);
-        let list = this.state.menuList.map((one, i) => {
-            if (!i) {
-                one.isExpand = !one.isExpand;
-            }
-            return one;
-        });
+    expandMenu = (obj, e) => {
+        let ev = e || window.event;
+        ev.stopPropagation();      
+        // console.log(obj.isExpand);
+        let list = this.getSelectMenu(this.state.menuList, obj.id);
         this.setState({
             menuList: list
         });
     }
 
+    getSelectMenu(list, id) {
+        console.log(id);
+        let arr = list.map( (one, i) => {
+            
+            if (one.id === id) {
+                if (one.children) {
+                    one.isExpand = !one.isExpand;
+                } else {
+                    one.isActive = true;
+                    console.log(one.url);
+                }
+            } else {
+                if (one.children) {
+                    this.getSelectMenu(one.children, id);
+                } else {
+                    one.isActive = false;
+                }
+            }
+            return one;         
+        });
+        return arr;
+    }
+
     render() {
         return (
             <div className="section-left">
-                <MenuItem menu={this.menu} />
+                <MenuItem list={this.state.menuList} onClickMenu={this.expandMenu} />
             </div>
         )
     }
 }
 
 class MenuItem extends Component {
+    list;
     constructor(props) {
         super(props);
-        this.state = {
-            menu: props.menu
-        }
-        console.log(this.state.menu.name);
+        console.log(props);
+        this.list = props.list;
     }
 
-    expandMenu(obj) {
-        // console.log(event);
-        // let ev = window.event;
-        // ev.stopPropagation();
-        // ev.nativeEvent.stopImmediatePropagation();
-        obj.isExpand = !obj.isExpand;
-        this.setState({
-            menu: obj
-        })
-    }
-
-    gotoPage(url, e) {
-        let ev = e || window.event;
-        ev.stopPropagation();
-        console.log(url);
-    }
+    
 
     render() {
-        const list = this.state.menu.children;
-        console.log(list);
-        let divList = "";
-        if (list && list.length) {
-            divList = list.map((one, i) => {
-                return (               
-                    <MenuItem menu={one} key={i} />
-                )
-            });
-        }        
-        return(
-            <div>
-                {!this.state.menu.name
-                    ? divList
-                    :
-                    <div>
-                    {this.state.menu.children
+        const menu = this.list.map( (one, i) => {
+            return (
+                <div key={i} className={one.isActive ? "active" : ""} onClick={(e) => this.props.onClickMenu(one, e)}>
+                    {one.name}
+                    {one.isExpand
                         ?
-                        <div onClick={ () => this.expandMenu(this.state.menu)}>
-                            {this.state.menu.name}
-                            {this.state.menu.isExpand
-                                ? divList
-                                : null 
-                            }                                               
-                        </div>
-                        :
-                        <div onClick={ (e) => this.gotoPage(this.state.menu.url, e)}>
-                            {this.state.menu.name}
-                        </div>
+                        <div className="child-menu">
+                            <MenuItem list={one.children} onClickMenu={this.props.onClickMenu} />
+                        </div>                        
+                        : null
                     }
-                    </div>
-                    
-                    
-                }
-            </div>           
-        )
+                </div>
+            )
+        });
+        return menu;
     }
 }
 
