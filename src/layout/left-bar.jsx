007f8938575c19d84b0995b1bf5626f82a17ee63
feiqi80 <feiq@microseer.com>
2018-09-17 22:10:41 +0800

    commit

--- src/layout/left-bar.jsx
+++ src/layout/left-bar.jsx
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 class Leftbar extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    console.log(props);    
     this.state = {
       menuList: [
         { id: 1, name: "菜单1", url: "", isExpand: false, children: [
@@ -12,11 +12,25 @@ class Leftbar extends Component {
           ]
         },
         { id: 3, name: "菜单2", isActive: false, url: "/calc" },
-        { id: 4, name: "菜单3", isActive: false, url: "/game" }
+        { id: 4, name: "菜单3", isActive: false, url: "/game" },
+        { id: 5, name: "菜单6", url: "", isExpand: false, children: [
+          {id: 6, name: "菜单6的子菜单", isActive: false, url: "/intro/:id"},
+        ]
+      },
       ]
-    }
+    };
+    
   }
 
+  componentWillMount() {
+    console.log("将要加载页面");
+    if (this.props.location.pathname.length > 1) {
+      let list = this.getSelectMenu(this.state.menuList, this.props.location.pathname, 1);
+      this.setState({
+        menuList: list
+      });
+    }
+  }
   componentWillReceiveProps(nextProps) {
     console.log(nextProps);
     const locationChanged = nextProps.location !== this.props.location;
@@ -26,23 +40,28 @@ class Leftbar extends Component {
   expandMenu = (obj, e) => {
     let ev = e || window.event;
     ev.stopPropagation();
-    let list = this.getSelectMenu(this.state.menuList, obj.id);
+    let list = this.getSelectMenu(this.state.menuList, obj.url);
     this.setState({
       menuList: list
     });
   }
 
-  getSelectMenu(list, id) {
+  getSelectMenu(list, url, isRefresh, upObj) {
     let arr = list.map((one, i) => {
-      if (one.id === id) {
+      if (one.url === url) {
         if (one.children) {
-          one.isExpand = !one.isExpand; 
+          one.isExpand = !one.isExpand;
         } else {
           one.isActive = true;
-          this.props.history.replace(one.url);
+          if (upObj) {
+            upObj.isExpand = true;
+          }
+          if (!isRefresh) {
+            this.props.history.replace(one.url);
+          }          
         }
       } else {
-        one.children ? this.getSelectMenu(one.children, id) : one.isActive = false;
+        one.children ? this.getSelectMenu(one.children, url, isRefresh, one) : one.isActive = false;
       }
       return one;
     });
